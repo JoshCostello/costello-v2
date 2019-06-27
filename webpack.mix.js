de4ee68e63cbc37d0cad4e64ca1e075752d0dee9
Joshua Costello <costeljm@gmail.com>
2019-06-27 19:46:52 -0400

    feat: configures Tailwind CSS

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,5 +1,6 @@
 let mix = require('laravel-mix');
 let build = require('./tasks/build.js');
+let tailwind = require('tailwindcss');
 
 mix.disableSuccessNotifications();
 mix.setPublicPath('source/assets/build');
@@ -7,12 +8,19 @@ mix.webpackConfig({
     plugins: [
         build.jigsaw,
         build.browserSync(),
-        build.watch(['source/**/*.md', 'source/**/*.php', 'source/**/*.scss', '!source/**/_tmp/*']),
-    ]
+        build.watch([
+            'source/**/*.md',
+            'source/**/*.php',
+            'source/**/*.scss',
+            '!source/**/_tmp/*',
+        ]),
+    ],
 });
 
 mix.js('source/_assets/js/main.js', 'js')
     .sass('source/_assets/sass/main.scss', 'css')
     .options({
         processCssUrls: false,
-    }).version();
+        postCss: [tailwind('./tailwind.config.js')],
+    })
+    .version();
