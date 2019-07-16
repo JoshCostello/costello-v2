b7cd41ba8d9dc97decf3b395250320e3a7cc1ce4
Joshua Costello <costeljm@gmail.com>
2019-07-16 19:59:22 -0400

    feat: add a shared header

---
+++ source/_layouts/shared/header.blade.php
@@ -0,0 +1,7 @@
+<header class="fixed w-full shadow px-12 md:px-20 pt-6 md:pb-6 site-header z-20">
+    <nav class="flex flex-wrap md:justify-between md:items-center">
+        <div class="mr-24">
+            <a class="font-mono border-4 border-salmon flex text-xl font-semibold" href="/"><span class="bg-salmon px-1 pr-2 text-white">joshua</span> <span class="px-1 text-gray-600">costello</span></a>
+        </div>
+    </nav>
+</header>
