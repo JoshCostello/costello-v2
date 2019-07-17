62ab3e6d331f51cb9b0d7818dbe7a9dbf6b93ed9
Joshua Costello <costeljm@gmail.com>
2019-07-16 20:41:26 -0400

    feat: run PurgeCSS when building for production

--- webpack.mix.js
+++ webpack.mix.js
@@ -20,6 +20,18 @@ mix.js('source/_assets/js/main.js', 'js')
     .sass('source/_assets/sass/main.scss', 'css')
     .options({
         processCssUrls: false,
-        postCss: [tailwind('./tailwind.config.js')],
+        postCss: [
+            tailwind('./tailwind.config.js'),
+            process.env.NODE_ENV === 'production' &&
+                require('@fullhuman/postcss-purgecss')({
+                    content: [
+                        './source/_layouts/**/*.blade.php',
+                        './source/**/*.blade.php',
+                        './source/index.blade.php',
+                    ],
+                    defaultExtractor: content =>
+                        content.match(/[A-Za-z0-9-_:/]+/g) || [],
+                }),
+        ],
     })
     .version();
